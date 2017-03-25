0c8a468f2c34b1c6bbb6487e75d3b15edecf9d05
nida_001 <nida3001@gmail.com>
2017-03-25 15:12:15 +0900

    gulp html

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var html_validator = require('gulp-html');
 
 var paths = {
-    'scss': 'src/scss/',
-    'css': 'dist/css/'
+    scss: 'src/scss/',
+    css: 'dist/css/',
+    html_src: 'src/html',
+    html_dst: 'dist/html'
 }
 
 gulp.task('scss', function() {
@@ -14,4 +17,10 @@ gulp.task('scss', function() {
         .on('error', sass.logError)
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.css))
+});
+
+gulp.task('html', function() {
+    return gulp.src(paths.html_src + '**/*.html')
+        // .pipe(html_validator()) // TODO あとでvalidにする
+        .pipe(gulp.dest(paths.html_dst))
 });
